6e14e6b3eff2a50e033dd49d943410368ce67139
huangkanhui <huangkanhui@ccssoft.com.cn>
2020-05-06 23:06:32 +0800

    kafei leetcode 最大的岛屿面积

--- src/router/modules/leetCode.js
+++ src/router/modules/leetCode.js
@@ -13,101 +13,154 @@ const leetCodeRouter = {
         icon: "chart"
     },
     children: [{
-        path: "array",
-        component: () =>
-            import("@/views/leetCode/index"),
-        name: "array",
-        meta: { title: "数组", noCache: true },
-        children: [{
-            path: "array1",
+            path: "array",
             component: () =>
-                import("@/views/leetCode/array/array1"),
-            meta: { title: "数组1", noCache: true }
-        },
-        {
-            path: "array2",
-            component: () =>
-                import("@/views/leetCode/array/array2"),
-            meta: { title: "数组2", noCache: true }
-        },
-        {
-            path: "array3",
-            component: () =>
-                import("@/views/leetCode/array/array3"),
-            meta: { title: "数组3", noCache: true }
-        },
-        {
-            path: "array4",
-            component: () =>
-                import("@/views/leetCode/array/array4"),
-            meta: { title: "数组4", noCache: true }
-        },
-        {
-            path: "array5",
-            component: () =>
-                import("@/views/leetCode/array/array5"),
-            meta: { title: "数组5", noCache: true }
-        },
-        {
-            path: "array6",
-            component: () =>
-                import("@/views/leetCode/array/array6"),
-            meta: { title: "数组6", noCache: true }
-        },
-        {
-            path: "array7",
-            component: () =>
-                import("@/views/leetCode/array/array7"),
-            meta: { title: "数组7", noCache: true }
-        }
-        ]
-    }, {
-        path: "String",
-        component: () =>
-            import("@/views/leetCode/index"),
-        name: "String",
-        meta: { title: "字符串", noCache: true },
-        children: [{
-            path: "string1",
-            component: () =>
-                import("@/views/leetCode/String/String1"),
-            meta: { title: "字符串1", noCache: true }
-        },
-        {
-            path: "string2",
-            component: () =>
-                import("@/views/leetCode/String/String2"),
-            meta: { title: "字符串2", noCache: true }
-        },
-        {
-            path: "string3",
-            component: () =>
-                import("@/views/leetCode/String/String3"),
-            meta: { title: "字符串3", noCache: true }
-        }
-        ]
-    },
-    {
-        path: "BinaryTree",
-        component: () =>
-            import("@/views/leetCode/index"),
-        name: "BinaryTree",
-        meta: { title: "二叉树", noCache: true },
-        children: [
-            {
-                path: "binaryTree1",
-                component: () =>
-                    import("@/views/leetCode/BinaryTree/BinaryTree1"),
-                meta: { title: "二叉树1", noCache: true }
+                import("@/views/leetCode/index"),
+            name: "array",
+            meta: {
+                title: "数组",
+                noCache: true
             },
-            {
-                path: "binaryTree2",
-                component: () =>
-                    import("@/views/leetCode/BinaryTree/BinaryTree2"),
-                meta: { title: "二叉树2", noCache: true }
-            }
-        ]
-    }
+            children: [{
+                    path: "array1",
+                    component: () =>
+                        import("@/views/leetCode/array/array1"),
+                    meta: {
+                        title: "数组1",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "array2",
+                    component: () =>
+                        import("@/views/leetCode/array/array2"),
+                    meta: {
+                        title: "数组2",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "array3",
+                    component: () =>
+                        import("@/views/leetCode/array/array3"),
+                    meta: {
+                        title: "数组3",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "array4",
+                    component: () =>
+                        import("@/views/leetCode/array/array4"),
+                    meta: {
+                        title: "数组4",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "array5",
+                    component: () =>
+                        import("@/views/leetCode/array/array5"),
+                    meta: {
+                        title: "数组5",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "array6",
+                    component: () =>
+                        import("@/views/leetCode/array/array6"),
+                    meta: {
+                        title: "数组6",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "array7",
+                    component: () =>
+                        import("@/views/leetCode/array/array7"),
+                    meta: {
+                        title: "数组7",
+                        noCache: true
+                    }
+                }
+            ]
+        }, {
+            path: "String",
+            component: () =>
+                import("@/views/leetCode/index"),
+            name: "String",
+            meta: {
+                title: "字符串",
+                noCache: true
+            },
+            children: [{
+                    path: "string1",
+                    component: () =>
+                        import("@/views/leetCode/String/String1"),
+                    meta: {
+                        title: "字符串1",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "string2",
+                    component: () =>
+                        import("@/views/leetCode/String/String2"),
+                    meta: {
+                        title: "字符串2",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "string3",
+                    component: () =>
+                        import("@/views/leetCode/String/String3"),
+                    meta: {
+                        title: "字符串3",
+                        noCache: true
+                    }
+                }
+            ]
+        },
+        {
+            path: "BinaryTree",
+            component: () =>
+                import("@/views/leetCode/index"),
+            name: "BinaryTree",
+            meta: {
+                title: "二叉树",
+                noCache: true
+            },
+            children: [{
+                    path: "binaryTree1",
+                    component: () =>
+                        import("@/views/leetCode/BinaryTree/BinaryTree1"),
+                    meta: {
+                        title: "二叉树1",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "binaryTree2",
+                    component: () =>
+                        import("@/views/leetCode/BinaryTree/BinaryTree2"),
+                    meta: {
+                        title: "二叉树2",
+                        noCache: true
+                    }
+                },
+                {
+                    path: "binaryTree3",
+                    component: () =>
+                        import("@/views/leetCode/BinaryTree/BinaryTree3"),
+                    meta: {
+                        title: "二叉树3",
+                        noCache: true
+                    }
+                }
+            ]
+        }
     ]
 };
 // eslint-disable-next-line eol-last
---
+++ src/views/leetCode/BinaryTree/BinaryTree3.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <p>给定一个包含了一些 0 和 1 的非空二维数组 grid 。</p>
+    <p>一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。</p>
+    <p>找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)</p>
+    <p>链接：https://leetcode-cn.com/problems/max-area-of-island</p>
+    <p>
+      [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+      [0,0,0,0,0,0,0,1,1,1,0,0,0],
+      [0,1,1,0,1,0,0,0,0,0,0,0,0],
+      [0,1,0,0,1,1,0,0,1,0,1,0,0],
+      [0,1,0,0,1,1,0,0,1,1,1,0,0],
+      [0,0,0,0,0,0,0,0,0,0,1,0,0],
+      [0,0,0,0,0,0,0,1,1,1,0,0,0],
+      [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+    </p>
+    <p>对于上面这个给定矩阵应返回 6。注意答案不应该是 11 ，因为岛屿只能包含水平或垂直的四个方向的 1 。</p>
+  </div>
+</template>
+
+<script>
+/* eslint-disable require-jsdoc */
+
+export default {
+  name: "BinaryTree3",
+  mounted () {
+    let grid = [
+      [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+      [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+    ];
+
+    var maxAreaOfIsland = function (grid) {
+      let result = 0;
+      for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+          if (grid[row][col] === 1) {
+            let count = dfs(row, col);
+            result = Math.max(result, count);
+          }
+        }
+      }
+      function dfs (row, col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === 0) {
+          return 0;
+        }
+        grid[row][col] = 0;
+        let count = 1;
+        count += dfs(row - 1, col);
+        count += dfs(row + 1, col);
+        count += dfs(row, col - 1);
+        count += dfs(row, col + 1);
+        return count;
+      }
+      return result;
+    };
+    console.log(maxAreaOfIsland(grid));
+  }
+};
+</script>
+
+<style>
+</style>
