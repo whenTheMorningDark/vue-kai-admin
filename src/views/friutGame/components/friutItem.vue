b38827ad8b912708a78196c2b8e471dd929d0459
Mr <404792402@qq.com>
2020-11-04 17:58:55 +0800

    kafei 提交运动函数代码

--- src/views/friutGame/components/friutItem.vue
+++ src/views/friutGame/components/friutItem.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="friutItem">
-    <img :src="item.src" width="100%" height="100%">
+  <div class="friutItem" :class="{active:item.active}">
+    <img :src="item.src" width="90%" height="90%">
+    <span class="rate">X{{item.rate}}</span>
   </div>
 </template>
 
@@ -20,6 +21,25 @@ export default {
 .friutItem{
   width:45px;
   height:45px;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  .rate{
+    // text-align: center;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    color: #666;
+  }
+  &.active{
+    background-color:red;
+    border-radius:4px;
+    .rate{
+      color: #ffffff;
+    }
+  }
   // padding-top:50%;
   // border-radius:4px;
   // border:1px solid #ddd;
--- src/views/friutGame/index.vue
+++ src/views/friutGame/index.vue
@@ -2,21 +2,9 @@
   <div class="friutGame z-h-100">
     <div class="friutGame-wrapper">
       <div class="friut-list" v-for="item in itemData"  :key="item.direction" :class="['direction'+item.direction]">
-      <friutItem v-for="sitem in item.children" :item="sitem" :key="sitem.name"></friutItem>
+        <friutItem v-for="sitem in item.children" :item="sitem" :key="sitem.name"></friutItem>
+      </div>
     </div>
-    </div>
-
-    <!-- <div class="toolbar">
-      <el-button size="mini" type="primary">编辑</el-button>
-      <el-button size="mini">其它功能</el-button>
-    </div>
-    <div class="previewDos">
-      <iframe
-      src="https://view.officeapps.live.com/op/view.aspx?src=http://hedaotest.gdpr.com/taskManager/a/comm/attachment/download/5357"
-      width="100%"
-      height="100%"
-    ></iframe>
-    </div> -->
   </div>
 </template>
 
@@ -31,13 +19,67 @@ export default {
     return {
       itemData: [
         { direction: "top", children: [
-          {name: "bigApple", src: require("./images/bigApple.png")},
-          {name: "smallApple", src: require("./images/smallApple.png")},
-          {name: "bigBanana", src: require("./images/bigBanana.png")},
-          {name: "smallBanana", src: require("./images/smallBanana.png")}
+          {name: "bigApple", src: require("./images/bigApple.png"), active: true, rate: 20},
+          {name: "smallApple", src: require("./images/smallApple.png"), rate: 10, active: false, },
+          {name: "bigBanana", src: require("./images/bigBanana.png"), rate: 25, active: false, },
+          {name: "smallBanana", src: require("./images/smallBanana.png"), rate: 15, active: false, }
+        ]},
+        { direction: "right", children: [
+          {name: "bigPear", src: require("./images/bigPear.png"), rate: 20, active: false, },
+          {name: "smallPear", src: require("./images/smallPear.png"), rate: 10, active: false, },
+          {name: "bigMelon", src: require("./images/bigMelon.png"), rate: 30, active: false, },
+        ]},
+        { direction: "bottom", children: [
+          {name: "smallMelon", src: require("./images/smallMelon.png"), rate: 15, active: false, },
+          {name: "bigOrange", src: require("./images/bigOrange.png"), rate: 20, active: false, },
+          {name: "smallOrange", src: require("./images/smallOrange.png"), rate: 10, active: false, }
+        ]},
+        { direction: "left", children: [
+          {name: "smallStar", src: require("./images/smallStar.png"), rate: 40, active: false, },
+          {name: "bigStar", src: require("./images/bigStar.png"), rate: 20, active: false, },
         ]}
-      ]
+      ],
+      friutData: [],
+      timer: null
     };
+  },
+  methods: {
+    // startIndex 起始运动的下表
+    // endIndex 截至的运动下标
+    targetMove(startIndex, endIndex, fn) {
+      return () => {
+        let direction = startIndex < endIndex;
+        let currentIndex = direction ? startIndex : endIndex;
+        clearInterval(this.timer);
+        this.timer = setInterval(() => {
+          if (currentIndex >= endIndex) {
+            clearInterval(this.timer);
+            fn && fn();
+          } else {
+            currentIndex = direction ? currentIndex + 1 : currentIndex - 1;
+            // startIndex++;
+            this.friutData.forEach(v => {
+              this.$set(v, "active", false);
+            });
+            this.$set(this.friutData[currentIndex], "active", true);
+          }
+        }, 200);
+      };
+    },
+    time(fn, n, context) {
+      return function() {
+        if (n--) {
+          return fn.apply(context || this, arguments);
+        }
+      };
+    }
+  },
+  mounted () {
+    this.friutData = this.itemData.reduce((cur, next) => cur.concat(...next.children), []);
+    // this.time(this.targetMove(-1, this.friutData.length - 1), 2);
+    var myFunction = this.time(this.targetMove(-1, this.friutData.length - 1), 3);
+    myFunction();
+    myFunction();
   }
 };
 </script>
@@ -46,17 +88,43 @@ export default {
 .friutGame-wrapper{
   width: 350px;
   height: 350px;
+  position: relative;
   margin:0 auto;
+  padding-top: 20px;
 }
-.friut-list{
+.directiontop{
   display:flex;
-  /* flex-direction:column; */
-  .friutItem{
-    flex: 1;
+}
+.directionright{
+  display:flex;
+  flex-direction:column;
+  position:absolute;
+  right:0;
+  top: 87.5px;
+}
+.directionbottom{
+  display:flex;
+  position:absolute;
+  bottom:0px;
+  width:350px;
+  height:87.5px;
+  flex-direction: row-reverse;
+  right: 87.5px;
+  // flex-direction:column-reverse;
+  // right:87.5px;
+}
+.directionleft{
+  display:flex;
+  flex-direction:column-reverse;
+  top: 87.5px;
+  left: 0px;
+}
+.friutItem{
+    width: 87.5px;
     height:87.5px;
     padding:10px;
   }
-}
+
 
 .toolbar{
   width: 100%;
